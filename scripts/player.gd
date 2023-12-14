59084558e51516a013d4285b2de997132553b61a
Riann <ooriannsilva@gmail.com>
2023-12-14 16:16:21 -0300

    Implemented basic Collisions, basic Ball and Enemy

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,16 @@
 extends CharacterBody2D
 
-@export var speed: int = 400
+@export var speed: int = 350
 
-func _process(delta):
+
+
+func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
-	position += direction * speed * delta
+	velocity = direction * speed 
+	move_and_slide()
+	
+	if Input.is_action_pressed("space_bar"):
+		rotation += 360 * delta
+	
+	if Input.is_action_just_released("space_bar"):
+		rotation = 0
---
+++ scripts/rect_enemy.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+@export var speed: int = 400
+
+var direction_up = Vector2.UP
+var direction_down = Vector2.DOWN
+
+
+func _process(delta):
+	pass
